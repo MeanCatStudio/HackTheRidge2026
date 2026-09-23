768d90edd57597ca263587f8ca4bbeacd3ee0e9f
Atharv Mahajan <125830249+Atharvamaj@users.noreply.github.com>
2026-09-23 14:19:36 -0400

    fix the web search

--- src/app/2025/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./2025.css";
-
-export default function Archive2025Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
--- src/app/2025/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Archive2025() {
-  redirect("/2025.html");
-}
